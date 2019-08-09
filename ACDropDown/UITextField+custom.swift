949068b3280131ee0653dfe2e3a93b3bbb3bdfe8
Anderson <asfcarvalho@me.com>
2019-08-09 08:20:03 -0300

    new version

--- ACDropDown/UITextField+custom.swift
+++ ACDropDown/UITextField+custom.swift
@@ -1,35 +1,72 @@
 //
-//  DropDownModel.swift
-//  Oncoclinicas
+//  UITextField+custom.swift
+//  GenBit
 //
-//  Created by Anderson Carvalho on 08/11/17.
-//  Copyright © 2017 asfcarvalho. All rights reserved.
+//  Created by Proaire on 01/07/19.
+//  Copyright © 2019 Treepart. All rights reserved.
 //
 
 import UIKit
+import DropDown
 
-@objc public protocol UITextFieldCustomProtocol {
-    func onTextDidChange(_ sender: UITextFieldCustom)
-    @objc optional func onTextDidEndEditing(_ sender: UITextFieldCustom)
-    func setErrorDropDown(_ isError: Bool)
+enum TextFieldBorderTypeEnum {
+    case Solid,
+    Bottom
 }
 
 public class UITextFieldCustom: UITextField {
     
+    fileprivate var anchorView: UIView!
+    fileprivate var dropDownInfo: DropDown?
+    fileprivate var dropDownList: [String]?
+    
+    public var dropDownDelegate: DropDownDelegate?
+    public var dropDownDataSource: DropDownDataSource?
+    
     static fileprivate let titleColorDefault = UIColor(white: 0.0, alpha: 0.92)
     
+    fileprivate var iconDefault: UIImageView?
+    
+    
+    
+    fileprivate let viewDropDown: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.clear
+        return view
+    }()
+    
     public let iconPassword: UIImageView? = {
-       let image = UIImageView()
-        image.image = UIImage(named: "iconEyeOpen")
-        image.highlightedImage = UIImage(named: "iconEyeHided")
-        return image
+        let imageView = UIImageView()
+        
+        let bundle = Bundle(for: UITextFieldCustom.self)
+        guard let image = UIImage(named: "iconEyeOpen", in: bundle, compatibleWith: nil) else {
+            fatalError("Missing iconEyeOpen...")
+        }
+        guard let highlightImage = UIImage(named: "iconEyeHided", in: bundle, compatibleWith: nil) else {
+            fatalError("Missing iconEyeHided...")
+        }
+        
+        imageView.image = image
+        imageView.highlightedImage = highlightImage
+        return imageView
+    }()
+    
+    public let iconDropDown: UIImageView = {
+        let imageView = UIImageView()
+        let bundle = Bundle(for: UITextFieldCustom.self)
+        guard let image = UIImage(named: "ic_dropDown", in: bundle, compatibleWith: nil) else {
+            fatalError("Missing ic_dropDown...")
+        }
+        
+        imageView.image = image
+        return imageView
     }()
     
     fileprivate var isShowPassword = false
     
-    public var textFieldCustomDelegate: UITextFieldCustomProtocol?
+    weak var textFieldCustomDelegate: UITextFieldCustomProtocol?
     
-    fileprivate var padding = UIEdgeInsets(top: 0, left: 5, bottom: 0, right: 5)
+    fileprivate var padding = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 5)
     
     /// To set the max number of text characters
     ///
@@ -41,15 +78,34 @@ public class UITextFieldCustom: UITextField {
     /// To set the mask pattern
     ///
     /// sample `(**) *****-****`
+    var countryCode = ""
     var formattingMaskPattern = ""
-    var replacementChar: Character = "*"
+    var replacementChar: Character = "9"
     var isCPFOrCNPJ: Bool = false
     var isJustCPF: Bool = false
-    var isDropDown: Bool = false
+    
+    
+    var borderType: TextFieldBorderTypeEnum = TextFieldBorderTypeEnum.Bottom {
+        didSet {
+            setupTextField()
+        }
+    }
     
     var isPassword: Bool = false {
         didSet {
-            setupIconPassword()
+            setupIcon()
+        }
+    }
+    
+    var isDropDown: Bool = false {
+        didSet {
+            setupIcon()
+        }
+    }
+    
+    var isDropDownEditable: Bool = false {
+        didSet {
+            setupIcon()
         }
     }
     
@@ -66,7 +122,7 @@ public class UITextFieldCustom: UITextField {
         let line = UIView()
         line.backgroundColor = self?.lineViewColor
         return line
-    }()
+        }()
     
     /// To set string of message to show on error
     /// by default `Error`
@@ -76,8 +132,9 @@ public class UITextFieldCustom: UITextField {
         let label = UILabel()
         label.text = self?.errorMessage //by default
         label.numberOfLines = 0
+        label.textAlignment = .right
         return label
-    }()
+        }()
     
     fileprivate dynamic var _title: String?
     fileprivate dynamic var _titleSize: CGFloat = 2
@@ -141,9 +198,9 @@ public class UITextFieldCustom: UITextField {
         return bounds.inset(by: padding)
     }
     
-    init(_ isDropDown: Bool = false) {
+    init() {
         super.init(frame: CGRect.zero)
-        self.isDropDown = isDropDown
+        
         setupTextField()
     }
     
@@ -167,18 +224,29 @@ public class UITextFieldCustom: UITextField {
     fileprivate func setupTextField() {
         
         self.font = UIFont.systemFont(ofSize: 16)
-        if !isDropDown {
+        self.backgroundColor = UIColor.clear
+        
+        self.addTarget(self, action: #selector(textDidChange), for: UIControl.Event.editingChanged)
+        self.addTarget(self, action: #selector(textDidBeginEditing), for: UIControl.Event.editingDidBegin)
+        self.addTarget(self, action: #selector(textDidEndEditing), for: UIControl.Event.editingDidEnd)
+        
+        
+        //MARK: Border type
+        if borderType == TextFieldBorderTypeEnum.Solid {
+            lineView.removeFromSuperview()
             self.layer.borderWidth = 1.0
             self.layer.borderColor = UIColor.black.cgColor
+        }else {
+            setupLineBottom(lineViewSize)
         }
+        
         self.returnKeyType = .next
         
         //MARK: Title
         setupTextFieldtitle()
         
-        NotificationCenter.default.addObserver(self, selector: #selector(textDidChange), name: NSNotification.Name(rawValue: "UITextFieldTextDidChangeNotification"), object: self)
-        NotificationCenter.default.addObserver(self, selector: #selector(textDidEndEditing), name: NSNotification.Name(rawValue: "UITextFieldTextDidEndEditingNotification"), object: self)
-        NotificationCenter.default.addObserver(self, selector: #selector(textDidBeginEditing), name: NSNotification.Name(rawValue: "UITextFieldTextDidBeginEditingNotification"), object: self)
+        
+        self.returnKeyType = .next
     }
     
     fileprivate func setupLineBottom(_ height: CGFloat = 0.5) {
@@ -201,41 +269,153 @@ public class UITextFieldCustom: UITextField {
         }
     }
     
-    //MARK: Setup password
-    fileprivate func setupIconPassword() {
+    //MARK: Dropdown configuration
+    fileprivate func configDropDown(_ anchorView: UIView? = nil) {
+        dropDownInfo = DropDown()
         
-        iconPassword?.removeFromSuperview()
+        if let anchorView = anchorView {
+            dropDownInfo?.anchorView = anchorView
+        }else {
+            let anchorView = UIView()
+            self.addSubview(anchorView)
+            anchorView.anchor(top: self.bottomAnchor, leading: self.leadingAnchor, bottom: self.bottomAnchor, trailing: self.trailingAnchor)
+            dropDownInfo?.anchorView = anchorView
+        }
         
-        self.addSubview(iconPassword ?? UIImageView())
+        dropDownInfo?.direction = .bottom
+        dropDownInfo?.dismissMode = .onTap
+        dropDownInfo?.bottomOffset = CGPoint(x: 0, y:(dropDownInfo?.anchorView?.plainView.bounds.height)!)
+        dropDownInfo?.topOffset = CGPoint(x: 0, y:-(dropDownInfo?.anchorView?.plainView.bounds.height)!)
         
-        iconPassword?.anchor(top: nil, leading: nil, bottom: nil, trailing: self.trailingAnchor, centerX: nil, centerY: self.centerYAnchor, padding: UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 8), size: CGSize(width: 15, height: 15))
+        dropDownInfo?.selectionAction = selected(_:_:)
         
-        let tapIconPassword = UITapGestureRecognizer(target: self, action: #selector(showPasswordAction))
-        iconPassword?.addGestureRecognizer(tapIconPassword)
-        iconPassword?.isUserInteractionEnabled = true
-        padding = UIEdgeInsets(top: 0, left: 5, bottom: 0, right: 25)
+        DropDown.appearance().selectionBackgroundColor = UIColor(red: 206, green: 206, blue: 206, alpha: 1)
         
-        showPasswordAction()
+        dropDownInfo?.cancelAction = {() in
+            self.hideAction()
+        }
+    }
+    
+    fileprivate func hideAction() {
+        if self.isDropDownEditable {
+            if let selectedIndex = self.dropDownInfo?.dataSource.firstIndex(of: self.text ?? "") {
+                self.selected(selectedIndex, self.dropDownInfo?.dataSource[selectedIndex] ?? "")
+                //                self.fadeInOut(true)
+            }else {
+                self.dropDownDelegate?.dropDownCanceled(self)
+                //                self.fadeInOut(true)
+            }
+        }
+        self.dropDownInfo?.hide()
+    }
+    
+    //MARK: Dropdown
+    public func reloadData() {
+        
+        if let numberOfRows = dropDownDataSource?.numberOfRows(self) {
+            
+            var list = [String]()
+            
+            for i in 0..<numberOfRows {
+                list.append(dropDownDataSource?.dropDown(self, itemByRow: i) ?? "")
+            }
+            
+            dropDownInfo?.dataSource = list
+            dropDownList = list
+            
+            
+            if self._placeHolder == nil {
+                DispatchQueue.main.async {
+                    self.text = list.first
+                    self.dropDownDelegate?.dropDownAction(self, 0, list.first ?? "")
+                }
+            }
+        }
+    }
+    
+    fileprivate func selected(_ index: Int, _ value: String) {
+        DispatchQueue.main.async {
+            self.text = value
+        }
+        let indexFilter = dropDownList?.firstIndex(of: value) ?? 0
+        dropDownDelegate?.dropDownAction(self, indexFilter, value)
+        //        fadeInOut(true)
+    }
+    
+    //MARK: Setup password or DropDown icon
+    fileprivate func setupIcon() {
+        
+        iconDefault?.removeFromSuperview()
+        viewDropDown.removeFromSuperview()
+        
+        padding = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        
+        if isPassword {
+            
+            iconDefault = iconPassword
+            
+            let tapIconPassword = UITapGestureRecognizer(target: self, action: #selector(showPasswordAction))
+            iconDefault?.addGestureRecognizer(tapIconPassword)
+            iconDefault?.isUserInteractionEnabled = true
+            padding = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 25)
+            
+            showPasswordAction()
+        }
+        
+        if isDropDown || isDropDownEditable {
+            iconDefault = iconDropDown
+            
+            let tapTextField = UITapGestureRecognizer(target: self, action: #selector(showDropDown))
+            
+            padding = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 25)
+            
+            viewDropDown.addGestureRecognizer(tapTextField)
+            viewDropDown.isUserInteractionEnabled = true
+            
+            self.addSubview(viewDropDown)
+            
+            viewDropDown.fillSuperview()
+            configDropDown()
+        }
+        
+        self.addSubview(iconDefault ?? UIImageView())
+        
+        iconDefault?.anchor(top: nil, leading: nil, bottom: nil, trailing: self.trailingAnchor, centerX: nil, centerY: self.centerYAnchor, padding: UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 8), size: CGSize(width: 15, height: 15))
+    }
+    
+    @objc fileprivate func showDropDown() {
+        
+        
+        if isDropDownEditable {
+            self.becomeFirstResponder()
+        }
+        
+        dropDownInfo?.show()
     }
     
     @objc fileprivate func showPasswordAction() {
-        iconPassword?.isHighlighted = !isShowPassword
+        iconDefault?.isHighlighted = !isShowPassword
         self.isSecureTextEntry = !isShowPassword
         isShowPassword.toggle()
     }
     
-    deinit {
-        NotificationCenter.default.removeObserver(self)
-    }
+    //    deinit {
+    //        notificationCenter.removeObserver(self)
+    //    }
     
     @objc fileprivate func textDidBeginEditing() {
-        setupErrorMessage(false, true)
-        lineView.backgroundColor = lineViewSelectedColor
+        setupErrorMessage(nil, true)
+        //        lineView.backgroundColor = lineViewSelectedColor
     }
     
     @objc fileprivate func textDidEndEditing() {
-        textFieldCustomDelegate?.onTextDidEndEditing?(self)
-        lineView.backgroundColor = lineViewSelectedColor
+        
+        if isDropDown || isDropDownEditable {
+            hideAction()
+        }else {
+            textFieldCustomDelegate?.onTextDidChange(self)
+        }
+        
     }
     
     fileprivate func fadeInOut(_ isIn: Bool) {
@@ -276,8 +456,6 @@ public class UITextFieldCustom: UITextField {
         if let text = text, text.count > 0 && formattingMaskPattern.count > 0 {
             let tempString = makeOnlyDigitsString(text)
             
-            
-            
             var finalText = ""
             var stop = false
             
@@ -312,8 +490,23 @@ public class UITextFieldCustom: UITextField {
             self.text = finalText
         }
         
-        if isEnabled {
-            textFieldCustomDelegate?.onTextDidChange(self)
+        if isDropDownEditable {
+            
+            setupErrorMessage(nil, true)
+            
+            dropDownInfo?.show()
+            if text?.isEmpty == true {
+                dropDownInfo?.dataSource = dropDownList ?? []
+                //                fadeInOut(false)
+            }else {
+                dropDownInfo?.dataSource = dropDownList?.filter({ (item) -> Bool in
+                    return item.lowercased().contains(text?.lowercased() ?? "")
+                }) ?? []
+                //                fadeInOut(true)
+            }
+            
+            
+            //            textFieldCustomDelegate?.onTextDidChange(self)
         }
         
     }
@@ -337,33 +530,45 @@ public class UITextFieldCustom: UITextField {
             formattingMaskPattern = "**.***.***/****-**"
         }
     }
-    
-    fileprivate func setupErrorMessage(_ isError: Bool, _ isSelected: Bool = false) {
+    //MARK: Error message
+    fileprivate func setupErrorMessage(_ isError: Bool? = nil, _ isSelected: Bool = false) {
         DispatchQueue.main.async {
             self._errorMessage.removeFromSuperview()
-            if isError {
+            if isError == true {
                 self._errorMessage.text = self.errorMessage
-                self._errorMessage.font = UIFont.systemFont(ofSize: self.errorFontSize)
+                self._errorMessage.font = UIFont(name: "SofiaPro-Regular", size: self.errorFontSize)
                 self._errorMessage.textColor = self.errorColor
-                if !self.isDropDown {
+                if self.borderType == TextFieldBorderTypeEnum.Solid {
                     self.layer.borderColor = self.errorColor.cgColor
                 }else {
-                    self.textFieldCustomDelegate?.setErrorDropDown(true)
+                    self.lineView.backgroundColor = self.errorColor
+                }
+                self.addSubview(self._errorMessage)
+                self.messageAnchor()
+            }else if isError == false {
+                if self.borderType == TextFieldBorderTypeEnum.Solid {
+                    self.layer.borderColor = self.lineViewColor.cgColor
+                }else {
+                    self.lineView.backgroundColor = self.lineViewSelectedColor
                 }
             }else {
-                if !self.isDropDown {
-                    self.layer.borderColor = UIColor.black.cgColor
+                if self.borderType == TextFieldBorderTypeEnum.Solid {
+                    self.layer.borderColor = self.lineViewColor.cgColor
                 }else {
-                    self.textFieldCustomDelegate?.setErrorDropDown(false)
+                    self.lineView.backgroundColor = self.lineViewColor
                 }
             }
-        }        
+        }
     }
     
     fileprivate func messageAnchor() {
         let messageSize = _errorMessage.textRect(forBounds: self.frame, limitedToNumberOfLines: 0)
-        let topMessage: CGFloat = -4
-        _errorMessage.anchor(top: self.bottomAnchor, leading: self.leadingAnchor, bottom: nil, trailing: self.trailingAnchor, padding: UIEdgeInsets(top: topMessage, left: 4, bottom: 0, right: 4), size: CGSize(width: 0, height: messageSize.height))
+        var top: CGFloat = 2
+        if self.borderType != TextFieldBorderTypeEnum.Solid {
+            top = 2
+        }
+        
+        _errorMessage.anchor(top: self.bottomAnchor, leading: self.leadingAnchor, bottom: nil, trailing: self.trailingAnchor, padding: UIEdgeInsets(top: top, left: 4, bottom: 0, right: 4), size: CGSize(width: 0, height: messageSize.height))
     }
     
     /// use to set error in the textfield
@@ -372,41 +577,30 @@ public class UITextFieldCustom: UITextField {
     /// - Paramters configuration:
     ///     - *errorFontSize* to set the error message font size
     ///     - *errorColor* to set the error message color
-    public func setError(_ isError: Bool, _ message: String? = nil) {
+    func setError(_ isError: Bool, _ message: String? = nil) {
         
         if let message = message {
             self.errorMessage = message
         }
         
-        if !isError {
-            
-            setupErrorMessage()
-            
-            _errorMessage.removeFromSuperview()
-            
-            self.addSubview(_errorMessage)
-            messageAnchor()
-            
-        }else {
-            _errorMessage.removeFromSuperview()
-            if !self.isDropDown {
-                self.layer.borderColor = UIColor.black.cgColor
-            }else {
-                self.textFieldCustomDelegate?.setErrorDropDown(false)
-            }
-        }
+        setupErrorMessage(!isError)
+        
     }
-    
-    
     
 }
 
-class RangeCustom {
-    
-    static func stringIndex(_ string: String, _ offsetBy: Int) -> String.Index {
-        
-        return string.index(string.startIndex, offsetBy: offsetBy)
-        
-    }
-    
+protocol UITextFieldCustomProtocol: NSObjectProtocol {
+    func onTextDidChange(_ sender: UITextFieldCustom)
+}
+
+//MARK: DropDown Protocol
+public protocol DropDownDelegate {
+    func dropDownAction(_ dropDown: UITextFieldCustom, _ index: Int, _ value: String)
+    func dropDownCanceled(_ dropDown: UITextFieldCustom)
+}
+
+@objc public protocol DropDownDataSource {
+    func numberOfRows(_ dropDown: UITextFieldCustom) -> Int
+    func dropDown(_ dropDown: UITextFieldCustom, itemByRow: Int) -> String
+    @objc optional func dropDownTitle(_ dropDown: UITextFieldCustom) -> String?
 }
